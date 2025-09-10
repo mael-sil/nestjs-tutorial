f01f6d5efdb782e393763629c8ce76f5ed2403da
mael-sil <silvestre-siaz.mael@protonmail.com>
2025-09-10 17:16:46 +0200

    Create service method and attribute for users and used it in users controller

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -8,31 +8,36 @@ import {
   Post,
   Query,
 } from '@nestjs/common';
+import { UsersService } from './users.service';
 
 @Controller('users')
 export class UsersController {
+  constructor(private readonly userService: UsersService) {}
+
   @Get()
   getAllUser(@Query('role') role?: 'ADMIN' | 'DEV' | 'INTERN') {
-    return [];
+    return this.userService.getAllUser(role)
   }
 
   @Get(':id')
   getOneUser(@Param('id') id: string) {
-    return { id };
+    return this.userService.getOneUser(+id)
   }
 
   @Post()
-  createUser(@Body() user: {}) {
-    return user;
+  createUser(@Body() user: {name: string, email: string ,role: 'ADMIN' | 'DEV' | 'INTERN'}) {
+    return this.userService.createUser(user)
   }
 
   @Patch(':id')
-  updateOneUser(@Param('id') id: string, @Body() userUpdate: {}) {
-    return { id, ...userUpdate };
+  updateOneUser(@Param('id') id: string, 
+  @Body() userUpdate: {name?: string, email?: string ,role?: 'ADMIN' | 'DEV' | 'INTERN'}
+  ) {
+    return this.userService.updateOneUser(+id, userUpdate)
   }
 
   @Delete(':id')
   deleteOneUser(@Param('id') id: string) {
-    return { id };
+    return this.userService.deleteOneUser(+id)
   }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,81 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+    private users = [
+        {
+            "id": 1,
+            "name": "Leanne Graham",
+            "email": "Sincere@april.biz",
+            "role": "DEV",
+        },
+        {
+            "id": 2,
+            "name": "Ervin Howell",
+            "email": "Shanna@melissa.tv",
+            "role": "INTERN",
+        },
+        {
+            "id": 3,
+            "name": "Clementine Bauch",
+            "email": "Nathan@yesenia.net",
+            "role": "ADMIN",
+        },
+        {
+            "id": 4,
+            "name": "Patricia Lebsack",
+            "email": "Julianne.OConner@kory.org",
+            "role": "DEV",
+        },
+        {
+            "id": 5,
+            "name": "Chelsey Dietrich",
+            "email": "Lucio_Hettinger@annie.ca",
+            "role": "ADMIN",
+        }
+    ]
+    private nextId: number = 6
+
+    getAllUser(role?: 'ADMIN' | 'DEV' | 'INTERN') {
+        if(role){
+            return this.users.filter(user => user.role === role)
+        }
+        return this.users
+    }
+
+    getOneUser(id: number){
+        return this.users.find(user => user.id === id)
+    }
+
+    createUser(user: {name: string, email: string ,role: 'ADMIN' | 'DEV' | 'INTERN'}){
+        const newUser= { "id": this.nextId, ...user}
+        this.nextId++
+        this.users.push(newUser)
+
+        return newUser
+    }
+
+    updateOneUser(id: number, userUpdate: {name?: string, email?: string ,role?: 'ADMIN' | 'DEV' | 'INTERN'}){
+        this.users = this.users.map(user => 
+            {
+                if(user.id === id){
+                    return {...user,...userUpdate}
+                }else{
+                    return user;
+                }
+                
+            }
+        )
+
+        
+
+        return this.getOneUser(id);
+    }
+
+    deleteOneUser(id: number){
+        const removedUser = this.getOneUser(id)
+        this.users = this.users.filter(user => user.id !== id)
+
+        return removedUser
+    }
+}
